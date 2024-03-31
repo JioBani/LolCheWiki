f5e1dd3d9d7696cc151b1ca26a97642d0949e203
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-04-01 00:56:36 +0900

    럭스 생성자 제거

--- lib/Model/MatchChampion.dart
+++ lib/Model/MatchChampion.dart
@@ -9,15 +9,6 @@ class MatchChampion{
 
   MatchChampion({required this.code , required this.koreanName, required this.cost , required this.imagePath});
 
-  factory MatchChampion.Lux(){
-      return MatchChampion(
-        code: "0",
-        koreanName: "럭스",
-        cost: 5,
-        imagePath: Images.championImage
-      );
-  }
-
   factory MatchChampion.fromUnitDto(UnitDto unitDto){
     return MatchChampion(
         code: unitDto.characterId,
